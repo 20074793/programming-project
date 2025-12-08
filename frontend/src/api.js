9c909a0d9beefb77893788533fd312892fccee93
20074793 <20074793@mydbs.ie>
2025-12-08 14:39:12 +0000

    Switch API calls to use apiClient instead of axios

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,44 +1,44 @@
-import axios from "axios";
+import apiClient from "./apiClient";
 
 // Fetch all ideas
 export const getIdeas = async () => {
-  const res = await axios.get("/api/ideas");
+  const res = await apiClient.get("/api/ideas");
   return res.data;
 };
 
 // Create a new idea
 export const createIdea = async (data) => {
-  const res = await axios.post("/api/ideas", data);
+  const res = await apiClient.post("/api/ideas", data);
   return res.data;
 };
 
 // Update an idea
 export const updateIdea = async (id, data) => {
-  const res = await axios.put(`/api/ideas/${id}`, data);
+  const res = await apiClient.put(`/api/ideas/${id}`, data);
   return res.data;
 };
 
 // Delete an idea
 export const deleteIdea = async (id) => {
-  const res = await axios.delete(`/api/ideas/${id}`);
+  const res = await apiClient.delete(`/api/ideas/${id}`);
   return res.data;
 };
 
 // Update idea status
 export const updateIdeaStatus = async (id, status) => {
-  const res = await axios.patch(`/api/ideas/${id}/status`, { status });
+  const res = await apiClient.patch(`/api/ideas/${id}/status`, { status });
   return res.data;
 };
 
 // like idea 
 export const likeIdea = async (id) => {
-  const res = await axios.post(`/api/ideas/${id}/like`);
+  const res = await apiClient.post(`/api/ideas/${id}/like`);
   return res.data;
 };
 
 // add comment to idea 
 export const addComment = async (id, commentData) => {
-  const res = await axios.post(`/api/ideas/${id}/comments`, commentData);
+  const res = await apiClient.post(`/api/ideas/${id}/comments`, commentData);
   return res.data;
 };
 
@@ -46,12 +46,12 @@ export const addComment = async (id, commentData) => {
 
 // Register a new user
 export const registerUser = async (data) => {
-  const res = await axios.post("/api/auth/register", data);
+  const res = await apiClient.post("/api/auth/register", data);
   return res.data;
 };
 
 // Login existing user
 export const loginUser = async (data) => {
-  const res = await axios.post("/api/auth/login", data);
+  const res = await apiClient.post("/api/auth/login", data);
   return res.data;
 };
